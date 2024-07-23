0b361b4d123f31f5cc3f63f9e62266c1ff503ac3
manticike <antwikisaac@gmail.com>
2024-07-23 05:42:03 +0000

    Created a Function That gets the player's choice
    This function uses the DOM to target the player's
    choices id and then display that on the screen
    
    I also add an even listener using the forEach loop
    on each button the player clicks and returns the
    player's choice

--- rps.js
+++ rps.js
@@ -1,6 +1,9 @@
-// Variables to keep track of the scores
 let humanScore = 0;
 let computerScore = 0;
+const playerButton = document.querySelectorAll('.playerChoice');
+
+
+playerButton.forEach(button => { button.addEventListener('click', getPlayerChoice) });
 
 // Getting computer's choice
 function getComputerChoice() {
@@ -20,72 +23,26 @@ function getComputerChoice() {
   return computerSelect;
 }
 
-// A function to get the human's choice
-function getHumanChoice(){
-  let playerInput = prompt("Enter one of these: rock, paper, scissors ");
-
-  // Convert the user input into a lowercase
-  let humanChoice = playerInput.toLowerCase();
-  return humanChoice;
-}
-
-const playerSelection = document.querySelector('#playerChoice');
-
-// Event listener to handle when a player clicks any button of his choice
-
-playerSelection.addEventListener('click', (event) => {
-  let target = event.target;
-
-  switch(target.id) {
-    case 'rock':
-      console.log('Human clicked the rock button');
-      break;
-    case 'paper':
-      console.log('Human clicked the paper button');
-      break;
-    case 'scissors':
-      console.log('Human clicked the scissors button');
-      break;
-  }
-});
 
 // A function to play a single round of the rps game
-function playRound(humanChoice, computerChoice) {
-  if(((humanChoice === rockBtn) && (computerChoice === 'scissors')) || ((humanChoice === paperBtn) && (computerChoice === 'rock')) || ((humanChoice === scissorsBtn) && (computerChoice === 'paper'))){
-    console.log(`You win! You chose ${humanChoice} and computer chose ${computerChoice}`);
+function playRound(playerSelection, computerChoice) {
+  if(((playerSelection === 'rock') && (computerChoice === 'scissors')) || ((playerSelection === 'paper') && (computerChoice === 'rock')) || ((playerSelection === 'scissors') && (computerChoice === 'paper'))){
+    console.log(`You win! You chose ${playerSelection} and computer chose ${computerChoice}`);
     humanScore++;
   }
   
-  else if (humanChoice === computerChoice) {
-    console.log(`It's a tie! You chose ${humanChoice} and computer chose ${computerChoice}`);
+  else if (playerSelection === computerChoice) {
+    console.log(`It's a tie! You chose ${playerSelection} and computer chose ${computerChoice}`);
   }
   
   else {
-    console.log(`You lose! you chose ${humanChoice} and computer chose ${computerChoice}`);
+    console.log(`You lose! you chose ${playerSelection} and computer chose ${computerChoice}`);
     computerScore++;
   }
 }
 
-const rockBtn = document.querySelector('#rock');
-const paperBtn = document.querySelector('#paper');
-const scissorsBtn = document.querySelector('#scissors');
-
-// rockBtn.addEventListener('click', function rockButton() {
-//   playRound()
-// });
-
-// I've commented this logic that plays the game 5 times. I'll come back later
-// A function to play the game 5 times
-// function playGame() {
-//   for(let i = 0; i < 5; i++){
-//     const computerSelection = getComputerChoice();
-//     let humanSelection = getHumanChoice();
-//     // Calling the function playRound to play the game 5 times
-//     console.log(playRound(humanSelection, computerSelection));
-//     console.log(`Your Score is: ${humanScore} and computer score is ${computerScore}`);
-//   } 
-// }
-
-playRound()
-// calling the function for it to run
-playGame();
+// A function to get the player's choice
+function getPlayerChoice(e) {
+  let playerSelection = (e.target.id);
+  playRound(playerSelection, getComputerChoice());
+}
